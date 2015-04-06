f982d091c18d861e0161e102517b6fa0bb13d6f2
Leandro Pessini <leandro@pessini.me>
2015-04-06 17:53:00 -0300

    Initial Commit

---
+++ MMInstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMInstagram
+//
+//  Created by Leandro Pessini on 4/6/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
