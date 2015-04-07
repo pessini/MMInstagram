886bc9d0b59691e39f73464fd58733371d326f08
Matt Larkin <mattlarkin@me.com>
2015-04-06 22:41:48 -0400

    added camera vidw controller

---
+++ MMInstagram/Camera.swift
@@ -0,0 +1,18 @@
+//
+//  Camera.swift
+//  MMInstagram
+//
+//  Created by Matt Larkin on 4/6/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+import UIKit
+
+class Camera: NSObject {
+
+
+
+
+    
+   
+}
