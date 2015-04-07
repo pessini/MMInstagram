c353907cbd70afd3cc9265ae8c45ce9eaf3f089d
Leandro Pessini <leandro@pessini.me>
2015-04-07 16:15:02 -0300

    Add new pod

--- MMInstagram/LoginViewController.swift
+++ MMInstagram/LoginViewController.swift
@@ -10,26 +10,31 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
+
+    @IBOutlet weak var signUpView: UIView!
+    @IBOutlet weak var logInView: UIView!
+
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.logInView.hidden = false
+        self.signUpView.hidden = true
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onLoginButtonTapped(sender: UIButton)
+    {
+        self.logInView.hidden = false
+        self.signUpView.hidden = true
     }
-    */
+
+    @IBAction func onSignUpButtonTapped(sender: UIButton)
+    {
+        self.logInView.hidden = true
+        self.signUpView.hidden = false
+    }
 
 }
